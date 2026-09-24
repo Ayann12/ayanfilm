f24414ff89e39e774e82f6b1325c9a21eee3dec0
Ayann12 <tantowi818@gmail.com>
2026-09-24 14:39:39 +0700

    memunculkan img dan rapihkan

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const API_KEY = "1ab7beeaa8458f65d10a2c22c4f07fda"
 let page = 1;
 
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`
+const API_IMAGE_URL = "https://image.tmdb.org/t/p/w1280"
 
 
 
@@ -18,7 +19,17 @@ function showMovies(movies) {
     movies.forEach(movie => {
         const { title, poster_path, overview } = movie
         const movieCard = document.createElement("div")
-        movieCard.innerHTML = `<p>${title}</p>`
+        movieCard.classList.add("movie")
+
+        movieCard.innerHTML = `
+        <img src="${API_IMAGE_URL + poster_path}" alt="html the movie image" />
+
+        <div class="detail">
+         <h3>${title}</h3>
+         <p>${overview.substring(0, 200)}...</p>
+        </div>
+        
+       `
         moviesElement.appendChild(movieCard)
     })
 
