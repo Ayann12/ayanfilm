e50be749f189c9157a5670cff77ea9d83c8a6f53
Ayann12 <tantowi818@gmail.com>
2026-09-24 14:21:50 +0700

    munculin title film

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 const API_KEY = "1ab7beeaa8458f65d10a2c22c4f07fda"
 
 let page = 1;
+
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`
 
 
 
 async function getMovies(url) {
     const res = await fetch(url)
-    const data = res.json()
-    console.log(data.results)
+    const data = await res.json()
+    showMovies(data.results)
 }
+
+
+function showMovies(movies) {
+    moviesElement.innerHTML = ''
+    movies.forEach(movie => {
+        const { title, poster_path, overview } = movie
+        const movieCard = document.createElement("div")
+        movieCard.innerHTML = `<p>${title}</p>`
+        moviesElement.appendChild(movieCard)
+    })
+
+}
+
+getMovies(API_URL)
